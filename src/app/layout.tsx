2a096abe254d18f2d4cfacbea46c732b5f285cb1
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-07-16 16:03:48 +0700

    changed website title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ean.dev",
+  title: "Ean James Yu",
   description: "Ean James Yu's portfolio",
 };
 
