8f86c8915d3d0f7e690a9a7699aea9de690b1f35
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-07-16 17:11:33 +0700

    made HeroCloudBackground.tsx and Contact.tsx text responsive for mobile

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
           Want to chat?
         </div>
         <div className="text-xl font-medium w-full mt-[30px] mb-[30px] flex flex-row max-md:flex-col items-center gap-y-[50px] gap-x-[50px] max-md:text-center">
-          <div className="w-[calc(35vh-10rem)]">
+          <div className="w-[calc(35vh-10rem)] min-w-[200px]">
             I&apos;m open to taking commissions for those who want their personal portfolio designed or any website in general. Everything ranging from designing, coding, deployment will be taken care of by me. If you do wish to work with me, contact me at:
           </div>
           <ContactWheel />
