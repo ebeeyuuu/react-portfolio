380208b1bc008e1047bc33badfba8a6337572da7
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-07-17 16:15:04 +0700

    optimised SEO performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Ean James Yu",
-  description: "Ean James Yu's portfolio",
+  title: "Ean James Yu - Portfolio",
+  description: "Explore Ean James Yu's portoflio showcasing creative design, photography, and more!",
 };
 
 export default function RootLayout({
@@ -17,6 +17,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      <meta name="description" content="Explore Ean James Yu's portfolio showcasing creative design, photography, and more!" />
+      <meta name="keywords" content="Ean James Yu, portfolio, creative design, photography, web development" />
+      <meta property="og:title" content="Ean James Yu - Portfolio" />
+      <meta property="og:description" content="Explore Ean James Yu's portfolio showcasing creative design, photography, and more!" />
+      <meta property="og:image" content="/portfolio-collage.jpg" />
+      <meta property="og:url" content="https://eanyu.vercel.app/" />
+      <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Ean James Yu - Portfolio" />
+      <meta name="twitter:description" content="Explore Ean James Yu's portfolio showcasing creative design, photography, and more!" />
+      <meta name="twitter:image" content="/portfolio-collage.jpg" />
+      <link rel="icon" href="/favicon.ico" />
       <body className={inter.className}>{children}</body>
     </html>
   );
