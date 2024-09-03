1b3b65247d8648ba2a78273dd2a8b6ba8df41c80
PK2Ean2 <ean@surabayaeuropeanschool.com>
2024-09-03 08:46:39 +0700

    updated ContactWheel.tsx to not refresh page upon copying discord ID

--- src/app/components/Contact/ContactWheel.tsx
+++ src/app/components/Contact/ContactWheel.tsx
@@ -1,59 +1,60 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import DiscordIcon from '../Icons/DiscordIcon'
-import GithubIcon from '../Icons/GithubIcon'
-import InstagramIcon from '../Icons/InstagramIcon'
-import TwitterIcon from '../Icons/TwitterIcon'
+import React, { useState } from "react";
+import DiscordIcon from "../Icons/DiscordIcon";
+import GithubIcon from "../Icons/GithubIcon";
+import InstagramIcon from "../Icons/InstagramIcon";
+import TwitterIcon from "../Icons/TwitterIcon";
 
 const ContactWheel = () => {
-  const [showCopyPopup, setShowCopyPopup] = useState(false)
-  
-  const handleCopyDiscordId = () => {
-    const discordId = '720463952706011159'; // Discord ID to copy
-    navigator.clipboard.writeText(discordId)
+  const [showCopyPopup, setShowCopyPopup] = useState(false);
+
+  const handleCopyDiscordId = (e) => {
+    e.preventDefault();
+    const discordID = "720463952706011159";
+    navigator.clipboard
+      .writeText(discordID)
       .then(() => {
-        setShowCopyPopup(true); // Show the copy popup
-        setTimeout(() => setShowCopyPopup(false), 2000); // Hide after 2 seconds
-        console.log('Discord ID copied to clipboard:', discordId);
+        setShowCopyPopup(true);
+        setTimeout(() => setShowCopyPopup(false), 2000);
       })
       .catch((error) => {
-        console.error('Failed to copy Discord ID:', error);
+        console.log("Failed to copy Discord ID: ", error);
       });
   };
-  
+
   return (
     <div className="main">
       <div className="up">
         <a href="https://www.instagram.com/ebee.yuuu/" target="_blank">
           <button className="card1">
-            <InstagramIcon className="text-black"/>
+            <InstagramIcon className="text-black" />
           </button>
         </a>
         <a href="https://x.com/ebeeyuuuu" target="_blank">
           <button className="card2">
-            <TwitterIcon className="text-black"/>
+            <TwitterIcon className="text-black" />
           </button>
         </a>
       </div>
       <div className="down">
         <a href="https://github.com/ebeeyuuu" target="_blank">
           <button className="card3">
-            <GithubIcon className="text-black"/>
+            <GithubIcon className="text-black" />
           </button>
         </a>
-        <a href="" onClick={handleCopyDiscordId}>
+        <a onClick={handleCopyDiscordId}>
           <button className="card4">
-            <DiscordIcon className="text-black"/>
+            <DiscordIcon className="text-black" />
           </button>
         </a>
       </div>
 
       {/* Copy Popup */}
       {showCopyPopup && (
-      <div className="copy-popup">
-        <p>Discord ID Copied!</p>
-      </div>
+        <div className="copy-popup">
+          <p>Discord ID Copied!</p>
+        </div>
       )}
 
       {/* Style JSX */}
@@ -81,7 +82,8 @@ const ContactWheel = () => {
           border: none;
           background: white;
           border-radius: 5px;
-          box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+          box-shadow:
+            rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
             rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
           transition: 0.5s ease-in-out;
           display: flex;
@@ -99,7 +101,7 @@ const ContactWheel = () => {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
 
-export default ContactWheel
+export default ContactWheel;
